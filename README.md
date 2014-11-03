cd2e10c7521fc93ec9c7c2563760093647e1f3ce
Anton Auoja <anton.auoja@tactel.se>
2014-11-03 09:34:06 +0100

    Added simple server as dev-dependency and updated readme

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+	$ npm install
+	$ node serve.js
+```
+
+[http://localhost:8080/demo/](http://localhost:8080/demo/)
---
+++ serve.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var ecstatic = require('ecstatic');
+
+http.createServer(
+    ecstatic({
+        root: __dirname
+    })
+).listen(8080);
+
+console.log('http://localhost:8080/demo');
